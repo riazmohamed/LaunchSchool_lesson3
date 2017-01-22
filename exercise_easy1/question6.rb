27f0f91f670d1943bf18f1fe8352455b3ad1e4d0
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-22 22:40:36 +0000

    add solution to question6

---
+++ exercise_easy1/question6.rb
@@ -0,0 +1,7 @@
+# question6.rb
+
+famous_words = "seven years ago..."
+
+puts "Four scores and " + famous_words
+puts "Four scores and " << famous_words
+puts famous_words.prepend('Four scores and ')
