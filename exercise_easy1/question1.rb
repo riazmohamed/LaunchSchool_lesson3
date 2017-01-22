9db06f035031875ae67a19682f19b6a836c27632
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-22 08:00:45 +0000

    add question.rb and organize folder

---
+++ exercise_easy1/question1.rb
@@ -0,0 +1,9 @@
+# question1
+
+numbers = [1, 2, 2, 3]
+numbers.uniq # Here the method 'uniq' does not mutate the original array
+
+puts numbers
+
+# this above program should print out 1, 2, 2, 3 in sequence in each line
+# the program returns nil as the method puts in the last line returns nil
