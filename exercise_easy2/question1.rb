daa9dde924c25cea0308605e5b4f34eb67c708aa
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-23 21:10:51 +0000

    Update solution for question1.rb

--- exercise_easy2/question1.rb
+++ exercise_easy2/question1.rb
@@ -5,3 +5,5 @@ ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
 p ages.has_key?("Spot")
 p ages.has_value?(ages["Spot"])
 p ages.key?("Spot")
+p ages.member?("Spot")
+p ages.include?("Spot")
