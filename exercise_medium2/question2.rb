132d6325bebf6d201dc162ebd2dea1c70dcc4e35
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-24 15:35:54 +0000

    add solution to question2.rb

---
+++ exercise_medium2/question2.rb
@@ -0,0 +1,105 @@
+# question2.rb
+
+def fun_with_ids
+  a_outer = 42
+  b_outer = "forty two"
+  c_outer = [42]
+  d_outer = c_outer[0]
+
+  a_outer_id = a_outer.object_id
+  b_outer_id = b_outer.object_id
+  c_outer_id = c_outer.object_id
+  d_outer_id = d_outer.object_id
+
+  puts "a_outer is #{a_outer} with an id of: #{a_outer_id} before the block."
+  puts "b_outer is #{b_outer} with an id of: #{b_outer_id} before the block."
+  puts "c_outer is #{c_outer} with an id of: #{c_outer_id} before the block."
+  puts "d_outer is #{d_outer} with an id of: #{d_outer_id} before the block.\n\n"
+
+  an_illustrative_method(a_outer, b_outer, c_outer, d_outer, a_outer_id, b_outer_id, c_outer_id, d_outer_id)
+
+
+  puts "a_outer is #{a_outer} with an id of: #{a_outer_id} BEFORE and: #{a_outer.object_id} AFTER the method call."
+  puts "b_outer is #{b_outer} with an id of: #{b_outer_id} BEFORE and: #{b_outer.object_id} AFTER the method call."
+  puts "c_outer is #{c_outer} with an id of: #{c_outer_id} BEFORE and: #{c_outer.object_id} AFTER the method call."
+  puts "d_outer is #{d_outer} with an id of: #{d_outer_id} BEFORE and: #{d_outer.object_id} AFTER the method call.\n\n"
+
+  puts "a_inner is #{a_inner} with an id of: #{a_inner_id} INSIDE and: #{a_inner.object_id} AFTER the method." rescue puts "ugh ohhhhh"
+  puts "b_inner is #{b_inner} with an id of: #{b_inner_id} INSIDE and: #{b_inner.object_id} AFTER the method." rescue puts "ugh ohhhhh"
+  puts "c_inner is #{c_inner} with an id of: #{c_inner_id} INSIDE and: #{c_inner.object_id} AFTER the method." rescue puts "ugh ohhhhh"
+  puts "d_inner is #{d_inner} with an id of: #{d_inner_id} INSIDE and: #{d_inner.object_id} AFTER the method.\n\n" rescue puts "ugh ohhhhh"
+end
+
+
+def an_illustrative_method(a_outer, b_outer, c_outer, d_outer, a_outer_id, b_outer_id, c_outer_id, d_outer_id)
+  a_outer_inner_id = a_outer.object_id
+  b_outer_inner_id = b_outer.object_id
+  c_outer_inner_id = c_outer.object_id
+  d_outer_inner_id = d_outer.object_id
+
+  puts "a_outer id was #{a_outer_id} before the method and is: #{a_outer.object_id} inside the method."
+  puts "b_outer id was #{b_outer_id} before the method and is: #{b_outer.object_id} inside the method."
+  puts "c_outer id was #{c_outer_id} before the method and is: #{c_outer.object_id} inside the method."
+  puts "d_outer id was #{d_outer_id} before the method and is: #{d_outer.object_id} inside the method.\n\n"
+
+  a_outer = 22
+  b_outer = "thirty three"
+  c_outer = [44]
+  d_outer = c_outer[0]
+
+  puts "a_outer inside after reassignment is #{a_outer} with an id of: #{a_outer_id} before and: #{a_outer.object_id} after."
+  puts "b_outer inside after reassignment is #{b_outer} with an id of: #{b_outer_id} before and: #{b_outer.object_id} after."
+  puts "c_outer inside after reassignment is #{c_outer} with an id of: #{c_outer_id} before and: #{c_outer.object_id} after."
+  puts "d_outer inside after reassignment is #{d_outer} with an id of: #{d_outer_id} before and: #{d_outer.object_id} after.\n\n"
+
+
+  a_inner = a_outer
+  b_inner = b_outer
+  c_inner = c_outer
+  d_inner = c_inner[0]
+
+  a_inner_id = a_inner.object_id
+  b_inner_id = b_inner.object_id
+  c_inner_id = c_inner.object_id
+  d_inner_id = d_inner.object_id
+
+  puts "a_inner is #{a_inner} with an id of: #{a_inner_id} inside the method (compared to #{a_outer.object_id} for outer)."
+  puts "b_inner is #{b_inner} with an id of: #{b_inner_id} inside the method (compared to #{b_outer.object_id} for outer)."
+  puts "c_inner is #{c_inner} with an id of: #{c_inner_id} inside the method (compared to #{c_outer.object_id} for outer)."
+  puts "d_inner is #{d_inner} with an id of: #{d_inner_id} inside the method (compared to #{d_outer.object_id} for outer).\n\n"
+end
+
+fun_with_ids
+
+# solution
+=begin
+a_outer is 42 with an id of: 85 before the block.
+b_outer is forty two with an id of: 70252189586240 before the block.
+c_outer is [42] with an id of: 70252189586200 before the block.
+d_outer is 42 with an id of: 85 before the block.
+
+a_outer id was 85 before the method and is: 85 inside the method.
+b_outer id was 70252189586240 before the method and is: 70252189586240 inside the method.
+c_outer id was 70252189586200 before the method and is: 70252189586200 inside the method.
+d_outer id was 85 before the method and is: 85 inside the method.
+
+a_outer inside after reassignment is 22 with an id of: 85 before and: 45 after.
+b_outer inside after reassignment is thirty three with an id of: 70252189586240 before and: 70252189585040 after.
+c_outer inside after reassignment is [44] with an id of: 70252189586200 before and: 70252189584960 after.
+d_outer inside after reassignment is 44 with an id of: 85 before and: 89 after.
+
+a_inner is 22 with an id of: 45 inside the method (compared to 45 for outer).
+b_inner is thirty three with an id of: 70252189585040 inside the method (compared to 70252189585040 for outer).
+c_inner is [44] with an id of: 70252189584960 inside the method (compared to 70252189584960 for outer).
+d_inner is 44 with an id of: 89 inside the method (compared to 89 for outer).
+
+a_outer is 42 with an id of: 85 BEFORE and: 85 AFTER the method call.
+b_outer is forty two with an id of: 70252189586240 BEFORE and: 70252189586240 AFTER the method call.
+c_outer is [42] with an id of: 70252189586200 BEFORE and: 70252189586200 AFTER the method call.
+d_outer is 42 with an id of: 85 BEFORE and: 85 AFTER the method call.
+
+ugh ohhhhh
+ugh ohhhhh
+ugh ohhhhh
+ugh ohhhhh
+=end
